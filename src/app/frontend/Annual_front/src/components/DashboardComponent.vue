64aa6537ee7e99bc83b44747cb5093671590d035
adrien <nordin_a@etna-alterance.net>
2025-01-02 18:16:44 +0100

    Feat : Update du dashboard

--- src/app/frontend/Annual_front/src/components/DashboardComponent.vue
+++ src/app/frontend/Annual_front/src/components/DashboardComponent.vue
@@ -1,42 +1,82 @@
 <template>
-    <q-card>
-      <q-card-section>
-        <div class="text-h5">Tableau de Bord</div>
-      </q-card-section>
-      <q-card-section>
-        <q-list bordered>
+    <q-card class="card">
+      <div class="dash_center q-pa-md">
+        <div class="text-h5 q-pa-md ">Tableau de Bord</div>
+        <q-list>
           <q-item clickable v-ripple to="/user">
             <q-item-section avatar>
               <q-icon name="account_circle" />
             </q-item-section>
             <q-item-section>Profil </q-item-section>
           </q-item>
+          
           <q-item clickable v-ripple to="/workspace">
             <q-item-section avatar>
               <q-icon name="workspaces" />
             </q-item-section>
             <q-item-section>Workspaces</q-item-section>
           </q-item>
-          <q-item clickable v-ripple to="/login">
+
+          <q-item clickable v-ripple to="/connexion">
             <q-item-section avatar>
               <q-icon name="login" />
             </q-item-section>
             <q-item-section>Connexion</q-item-section>
           </q-item>
-          <q-item clickable v-ripple to="/signup">
+
+          <q-item clickable v-ripple to="/documents">
+            <q-item-section avatar>
+              <q-icon name="folder" />
+            </q-item-section>
+            <q-item-section>Documents</q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple to="/chatbot">
+            <q-item-section avatar>
+              <q-icon name="smart_toy" />
+            </q-item-section>
+            <q-item-section>RaggyChat</q-item-section>
+          </q-item>
+
+          <!-- <q-item clickable v-ripple to="/connexion">
             <q-item-section avatar>
               <q-icon name="person_add" />
             </q-item-section>
             <q-item-section>Inscription</q-item-section>
-          </q-item>
+          </q-item> -->
+
         </q-list>
-      </q-card-section>
+      </div>
     </q-card>
   </template>
   
   <script>
   export default {
-    name: "DashboardNavigation",
+    name: "DashboardComponent",
   };
   </script>
+
+<style>
+.dash_center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100px; 
+  text-align: center;
+  margin-right: auto;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 350px;  
+  max-width: 250px;  
+  margin: 0 auto;  
+  background-color: #abf1ec;
+
+}
+</style>
   
--- src/app/frontend/Annual_front/src/router/routes.js
+++ src/app/frontend/Annual_front/src/router/routes.js
@@ -1,16 +1,22 @@
+
+import { createRouter, createWebHistory } from 'vue-router';
+import { useQuasar } from 'quasar';
+
 const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      // { path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '', component: () => import('pages/DashboarPage.vue') },
-      { path: 'test', component: () => import('pages/TestPage.vue') },
-      { path: '/user', component: () => import('pages/UserPage.vue') },
-      { path: 'workspace', component: () => import('pages/WorkspacePage.vue') },
+      { path: '', component: () => import('components/DashboardComponent.vue') },
+      { path: '/dashboard', component: () => import('components/DashboardComponent.vue') },
+      { path: '/test', component: () => import('components/HelloComponent.vue') },
+      { path: '/user', component: () => import('components/UserComponent.vue') },
+      { path: '/connexion', component: () => import('components/ConnexionComponent.vue') },
+      { path: '/workspace', component: () => import('components/WorkspaceComponent.vue') },
+      { path: '/documents', component: () => import('components/DocumentComponent.vue') },
+      // { path: '/chatbot', component: () => import('components/ChatComponent.vue') },
 
 
- 
     ]
   },
 
@@ -20,6 +26,25 @@ const routes = [
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
   }
-]
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+
+router.beforeEach((to, from, next) => {
+  const $q = useQuasar();
+  // Affiche le spinner global pendant le chargement
+  $q.loading.show();
+  next();
+});
+
+router.afterEach(() => {
+  const $q = useQuasar();
+  // Cache le spinner après la navigation
+  $q.loading.hide();
+});
 
 export default routes
